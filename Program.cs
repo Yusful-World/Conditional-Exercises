045dc88ab89fc72552936d9cfd51cca84abf23f8
Abdullahi Yusuf <abdullahiyusuf042000@gmail.com>
2024-07-21 22:50:49 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditional_Exercises
+{
+    public class Program
+    {
+        static void Main(string[] args)
+
+        //conditional exercise 2
+        {
+            Console.WriteLine("Enter a number");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter another number");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            var max = (number1 > number2) ? number1 : number2;
+            Console.WriteLine("The Max is " + max);
+        }
+        public void Exercise2()
+        {
+            
+            //condition exercise 1
+            Console.WriteLine("enter a number from 1 - 10");
+            string input = Console.ReadLine();
+            var number = Convert.ToInt32(input);
+            if (number > 0 && number <= 10)
+                Console.WriteLine("Valid");
+            else if (number <=0)
+                Console.WriteLine("Invalid");
+
+        }
+
+        
+        
+
+        
+    }
+}
